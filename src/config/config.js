86cc4798598f7143ce03996519b97ae0e0c71fb7
km-saifullah <kmsaifullah16@gmail.com>
2024-05-17 10:44:39 +0600

    config.js modified

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,11 @@
 const config = {
   apiKey: String(import.meta.env.VITE_FIREBASE_API_KEY),
   authDomain: String(import.meta.env.VITE_FIREBASE_AUTH_DOMAIN),
+  databaseURL: String(import.meta.env.VITE_FIREBASE_DATABASE_URL),
   projectId: String(import.meta.env.VITE_FIREBASE_PROJECT_ID),
   storageBucket: String(import.meta.env.VITE_FIREBASE_STORAGE_ID),
   messagingSenderId: String(import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID),
   appId: String(import.meta.env.VITE_FIREBASE_APP_ID),
-  databaseURL: String(import.meta.env.VITE_FIREBASE_DATABASE_URL),
 };
 
 export default config;
